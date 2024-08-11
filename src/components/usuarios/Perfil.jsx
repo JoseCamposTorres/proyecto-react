c881bdb5855b9d17a49443e9b4205dd326d78008
JoseCamposTorres <joseluiscampostorres10@gmail.com>
2024-08-10 22:39:14 -0500

    envio

--- src/components/usuarios/Perfil.jsx
+++ src/components/usuarios/Perfil.jsx
@@ -1,13 +1,13 @@
 // import { useEffect, useState } from "react"
 import { useContext } from "react"
 import { UserContext } from "../../context/UserContext"
-
+import "./Perfil.css"
 const Perfil = () => {
     const usuario = useContext(UserContext)
 
   return (
     <>
-      <div>
+      <div className="perfil">
         <h1>Perfil de {usuario.name}</h1>
         <div>Usuario desde : {usuario.registered}</div>
       </div>
